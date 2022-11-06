3ed74fc7e6253ed747306612e099f65406c3c555
Ryn Parin <ryn.parin@gmail.com>
2022-11-06 11:24:37 +0700

    added custom elastic hook

---
+++ plugins/hooks/elastic/elastic_hook.py
@@ -0,0 +1,39 @@
+from airflow.plugins_manager import AirflowPlugin
+from airflow.hooks.base import BaseHook
+
+from elasticsearch import Elasticsearch
+
+# Custom Hook
+
+# Create class inherit from BaseHook
+class ElasticHook(BaseHook):
+    def __init__(self, conn_id="elastic_default", *args, **kwargs):
+        # get connection
+        super().__init__(*args, **kwargs)
+        conn = self.get_connection(conn_id)
+
+        conn_config = {}
+        # can has multiple host
+        hosts = []
+
+        # if have a host, port, login
+        if conn.host:
+            hosts = conn.host.split(",")
+        if conn.port:
+            conn_config["port"] = int(conn.port)
+        if conn.login:
+            conn_config["http_auth"] = (conn.login, conn.password)
+
+        self.es = Elasticsearch(hosts, **conn_config)
+        self.index = conn.schema
+
+    def info(self):
+        return self.es.info()
+
+    def set_index(self, index):
+        self.index = index
+
+    def add_doc(self, index, doc_type, doc):
+        self.set_index(index)
+        res = self.es.index(index=index, doc_type=doc_type, doc=doc)
+        return res
